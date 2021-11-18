17ee9a47c6d1fa70d2983951aad99e43a1c38ce6
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-18 16:43:11 -0500

    Correct Linters errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
   def login
     if body_params[:username]
       @user = User.find_by(username: body_params[:username])
-      if @user 
+      if @user
         token = AuthenticateUser.new(body_params[:username]).call
         token['message'] = 'Logged in Succesfuly!'
         render json: token
