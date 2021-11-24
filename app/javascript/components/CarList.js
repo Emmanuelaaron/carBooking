9fe835ed56c6558d282539378d55825b592419be
jaar91 <jaarriagaramos@gmail.com>
2021-11-23 18:01:24 -0600

    add style movement to the home page

--- app/javascript/components/CarList.js
+++ app/javascript/components/CarList.js
@@ -6,7 +6,7 @@ import Car from './Car';
 
 const CarList = (props) => {
   const { cars } = props;
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
 
   const pageLeftBtn = () => {
     setPage(page - 1);
@@ -18,30 +18,38 @@ const CarList = (props) => {
 
   return (
     <Row className="m-0 p-0 h-100 position-relative">
-      <button
-        className={"position-absolute top-50 start-0 translate-middle-y " + (page === 1 ? "home-left-disabled" : "home-left")}
-        onClick={pageLeftBtn} disabled={ page === 1 ? true : false}
-      >
-            <img className="arrowicon" src={triangleArrowImg}/>
-      </button>
       <Col md="12" className="flex-grow">
         <div className="d-flex flex-column justify-content-center h-100">
           <div className="w-100 text-center">
             <h2>LATEST MODELS</h2>
             <p className="text-muted">Please select a model</p>
           </div>
-          <div className="d-flex flex-row justify-content-around">
-            {
-              cars.filter((val, index) => index >= ((page * 3) - 3) && index < (page * 3)).map((car) => {
-                return <Car key={car.id} car={car} />;
-              })
-            }
+          <div className="slider-container">
+            <div
+              className="d-flex"
+              style={{transform: `translateX(-${page * 1311}px)`, transition: "transform 0.3s ease-out"}}
+            >
+              {
+                cars.map((car) => {
+                  return <Car
+                    key={car.id}
+                    car={car}
+                  />;
+                })
+              }
+            </div>
           </div>
         </div>
       </Col>
       <button
-        className={"position-absolute top-50 end-0 translate-middle-y " + (page === (cars.length/3) ? "home-rigth-disabled" : "home-rigth")}
-        onClick={pageRigthBtn} disabled={ page === (cars.length/3) ? true : false }
+        className={"d-none d-md-block position-md-absolute top-50 start-0 translate-middle-y " + (page === 0 ? "home-left-disabled" : "home-left")}
+        onClick={pageLeftBtn} disabled={ page === 0 ? true : false}
+      >
+            <img className="arrowicon" src={triangleArrowImg}/>
+      </button>
+      <button
+        className={"position-absolute top-50 end-0 translate-middle-y " + (page === (cars.length/3) -1 ? "home-rigth-disabled" : "home-rigth")}
+        onClick={pageRigthBtn} disabled={ page === (cars.length/3) -1 ? true : false }
       >
             <img className="arrowicon-inverse" src={triangleArrowImg}/>
       </button>
