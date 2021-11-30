8df538034f779cdac736008dd225b5fcca3a54a3
jaar91 <jaarriagaramos@gmail.com>
2021-11-30 14:47:45 -0600

    create test for models user and cars

---
+++ spec/models/car_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Car, type: :model do
+  it 'car require name, model, description, price' do
+    Car.create(name: "Ford", model:"mustang", description: "1971 black with silver line", price: 450000 )
+    expect(Car.all.length).to eq(1)
+  end
+end
