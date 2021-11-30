f53fc60ac23762a41a19a298bdb183668b1f9333
jaar91 <jaarriagaramos@gmail.com>
2021-11-30 14:04:26 -0600

    disable rubocop for method imageData due to react require camel case for linterse to pass

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    if body_params[:username] != ""
+    if body_params[:username] == ''
+      render json: { message: 'User information not sent!', code: 204 }
+    else
       @user = User.new(username: body_params[:username])
       if @user.save
         token = AuthenticateUser.new(body_params[:username]).call
@@ -12,13 +14,13 @@ class Api::V1::UsersController < ApplicationController
       else
         render json: { message: 'User is already in use', code: 409 }
       end
-    else
-      render json: { message: 'User information not sent!', code: 204 }
     end
   end
 
   def login
-    if body_params[:username] != ""
+    if body_params[:username] == ''
+      render json: { message: 'User information not sent!', code: 204 }
+    else
       @user = User.find_by(username: body_params[:username])
       if @user
         token = AuthenticateUser.new(body_params[:username]).call
@@ -28,8 +30,6 @@ class Api::V1::UsersController < ApplicationController
       else
         render json: { message: 'Invalid username', code: 2001 }
       end
-    else
-      render json: { message: 'User information not sent!', code: 204 }
     end
   end
 
--- app/models/car.rb
+++ app/models/car.rb
@@ -3,12 +3,13 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy, class_name: 'Reservation', foreign_key: 'car_id'
   attribute :imageData
 
-  def imageData
-    image_url
-  end
-  
   validates :name, presence: true
   validates :model, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  # rubocop:disable all
+  def imageData
+    image_url
+  end
+  # rubocop:enable all
 end
