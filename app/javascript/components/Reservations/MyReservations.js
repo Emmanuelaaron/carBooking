e572440a63506d254eecb056e695946199b0a1ac
petudeveloper <davidalvarezmazzo@gmail.com>
2021-12-01 12:19:20 -0500

    Add no reservations message.

--- app/javascript/components/Reservations/MyReservations.js
+++ app/javascript/components/Reservations/MyReservations.js
@@ -24,28 +24,37 @@ function MyReservations() {
     <div className={styles.container}>
         <h2 className={styles.title}>My Cars Reservations</h2>
         <ul className={styles.list}>
-          { myReservations.map((reservation) => {
-            const car = cars.find((car) => car.id === reservation.car_id);
-            const city = cities.find((city) => city.id === reservation.city_id);
-            const { date } = reservation;
-            return (
-              <li key={reservation.id} className={styles.card}>
-                <div className={styles.lefftInfo}>
-                  <p className={styles.carName}>{car.name}</p>
-                  <p className={styles.carModel}>{car.model.toUpperCase()}</p>
-                  <p className={styles.city}>{city.name}</p>                  
-                </div>
-                <div className={styles.imageContainer}>
-                  <img className={styles.carImg} src={car.imageData} alt={car.name} />
-                </div>
-                <div className={styles.rightInfo}>
-                  <p className={styles.carDescription}>{car.description.toUpperCase()}</p>
-                  <p className={styles.date}>{date}</p>
-                  <button type="button" className={styles.button} onClick={() => handleClick(reservation.id)}>DELETE RESERVATION</button>
-                </div>
+          { 
+            myReservations.length > 0 ?
+            myReservations.map((reservation) => {
+              const car = cars.find((car) => car.id === reservation.car_id);
+              const city = cities.find((city) => city.id === reservation.city_id);
+              const { date } = reservation;
+              return (
+                <li key={reservation.id} className={styles.card}>
+                  <div className={styles.lefftInfo}>
+                    <p className={styles.carName}>{car.name}</p>
+                    <p className={styles.carModel}>{car.model.toUpperCase()}</p>
+                    <p className={styles.city}>{city.name}</p>                  
+                  </div>
+                  <div className={styles.imageContainer}>
+                    <img className={styles.carImg} src={car.imageData} alt={car.name} />
+                  </div>
+                  <div className={styles.rightInfo}>
+                    <p className={styles.carDescription}>{car.description.toUpperCase()}</p>
+                    <p className={styles.date}>{date}</p>
+                    <button type="button" className={styles.button} onClick={() => handleClick(reservation.id)}>DELETE RESERVATION</button>
+                  </div>
+                </li>
+              );
+            })
+            :
+              <li className={styles.card}>
+                <p className={styles.noReservation}>No</p>
+                <p className={styles.noReservation}>Reservations</p>
+                <p className={styles.noReservation}>Found</p>
               </li>
-            );
-          })}
+          }
         </ul>
         <p className={styles.subtitle}>This is a list of all the cars you have reserve</p>
       </div>
